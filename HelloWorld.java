4063d654277de4cc4da9e06a65623005103f0afb
prince <princeihe123@gmail.com>
2023-10-02 12:03:33 +0100

    branching exercise

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        for(int i = 1; i < 11; i++) {
-            System.out.println(i);
-        }
+        System.out.println("This is another string!");
     }
 }
