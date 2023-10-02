43fe857e237b1dbaf9439c1f6c1a11f5ce366ca3
prince <princeihe123@gmail.com>
2023-10-02 12:08:06 +0100

    calculates average

---
+++ average.java
@@ -0,0 +1,11 @@
+public class average {
+    public static void main(String[] args) {
+        float a = 5;
+        float b = 2;
+        float c = 39;
+        float average;
+
+        average = (a + b + c) / 3;
+        System.out.println(average);
+    }
+}
